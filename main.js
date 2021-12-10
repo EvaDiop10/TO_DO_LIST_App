ad165856d5d1d4199a81c1bf787980759f4c4386
thiareMohamed <thiaremohamed.mt@gmail.com>
2021-12-10 11:07:46 +0000

    Refactorie responsive

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// connexion et deconnexion
+
 const cnx = document.querySelector('.connexion');
 const deCnx = document.querySelector('.deconnexion');
 
@@ -15,3 +17,47 @@ togglerBtnIns.addEventListener('click', (e) =>{
         cnx.hidden = false;
         deCnx.hidden = true;
 })
+
+// Recureration des donnees de la connexion
+
+const email = document.querySelector('.mail');
+const motDePasse = document.querySelector('.mdp');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function validate() { 
+//     var msg; 
+//     var str = document.getElementById("mdp").value; 
+//     if (str.match( /[0-9]/g) && 
+//             str.match( /[A-Z]/g) && 
+//             str.match(/[a-z]/g) && 
+//             str.match( /[^a-zA-Z\d]/g) &&
+//             str.length >= 10) 
+//         msg = "<p style='color:green'>Mot de passe fort.</p>"; 
+//     else 
+//         msg = "<p style='color:red'>Mot de passe faible.</p>"; 
+//     document.getElementById("msg").innerHTML= msg; 
+// } 
