a77ee2caf5f662ffb48dec75d5abcbdfb34611cd
thiam <lmtlibasse@saloumpharma.com>
2021-12-13 09:44:17 +0000

    api.js mise a jour

--- todoliste/api.js
+++ todoliste/api.js
@@ -27,3 +27,63 @@ ajouter.addEventListener('click', (e)=>{
   }
 })
 
+//Referencer la collection dans firebase
+  
+/* const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+var tacheRef = db.database().ref('tache');
+ */
+
+
+
+//form submit 
+document.getElementById('myForm').addEventListener('submit', sauvegarder)
+
+function sauvegarder(e){
+    e.preventDefault()
+
+    //recuperer valeur champs
+
+    var titre = recupererChamps('titre');
+    var date = recupererChamps('date');
+    var description = recupererChamps('description');
+
+    enregistreTache(titre, date, description)
+   
+
+    
+}
+// fonction pour recuperer valeurs des champs 
+
+function recupererChamps(id){
+
+    return document.getElementById(id).value; 
+}
+
+
+//fonction pour enregistrer la tache dans firebase
+
+function enregistrerDonnee(titre, date, description){
+    tacher
+}
+
+
+
+
+ 
+
+// fonction pour recuperer valeurs des champs 
+
+
+
+function enregistreTache(titre, date, description){
+
+var nouvelleTacheRef = {
+    titre : titre,
+    date : date, 
+    description :  description
+}
+console.log(nouvelleTacheRef)
+}
+
